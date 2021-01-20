37b5041135d4bc85eac0c517d3b225fa5317f512
Grammy Leung <grammyleung@gmail.com>
2021-01-21 04:13:12 +0800

    .NET 5 小白全栈（6）初识变量与数据类型 - 啥是变量

---
+++ FirstFullStack/BackEnd/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ValuesController : ControllerBase
+	{
+		[HttpGet]
+		[Route("[action]")]
+		public dynamic GetValue()
+		{
+			return 1;
+		}
+	}
+}
