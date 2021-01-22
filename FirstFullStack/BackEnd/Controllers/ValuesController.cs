01630cbf4a705efb2072022abfb5960809abd8f5
Grammy Leung <grammyleung@gmail.com>
2021-01-23 01:00:36 +0800

    .NET 5 小白全栈（7）数字相关的数据类型 - 数字的运算

--- FirstFullStack/BackEnd/Controllers/ValuesController.cs
+++ FirstFullStack/BackEnd/Controllers/ValuesController.cs
@@ -50,8 +50,15 @@ namespace BackEnd.Controllers
 		public int GetInteger()
 		{
 			int valueInteger;
-			valueInteger = 54; // 一副扑克牌的数目
-			return valueInteger;
+			valueInteger = 54 - 2; // 一副扑克牌的数目，减去两张大小鬼。
+			valueInteger = valueInteger / 4; // 把除去大小鬼之后的牌，平均分给四个人，玩锄大地。
+			/*
+			以上这行可以简写成：
+			valueInteger /= 4; 
+			意思就是这个变量把自己除以四，再赋值给自己。
+			（在C语言中，超过一行的注释可以用斜杠星和星斜杠来形成多行的注释段落。）
+			*/
+			return valueInteger; // 返回每个人能够分得多少张牌。玩过的人都知道是13张，调试一下对不对。
 		}
 
 		[HttpGet]
@@ -59,7 +66,13 @@ namespace BackEnd.Controllers
 		public decimal GetDecimal()
 		{
 			decimal valueDecimal;
-			valueDecimal = 8848.86m; // 珠穆朗玛峰的最新海拔
+
+			// 珠穆朗玛峰的最新海拔
+			valueDecimal = 8848.86m;
+
+			// 珠穆朗玛峰平均每一百年上升7厘米，求2046年的时候，珠穆朗玛峰高度是多少？
+			valueDecimal += 0.07m * (2046 - 2021) / 100;
+
 			return valueDecimal;
 		}
 	}
